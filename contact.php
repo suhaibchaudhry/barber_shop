80998778e64000017b4d15dea1b3507d2f793997
Suhaib Chaudhry <chaudhry.suhaib@gmail.com>
2019-11-19 11:37:48 -0600

    initial commit

---
+++ contact.php
@@ -0,0 +1,7 @@
+<?php
+  $data = "First Name: " . $_POST['firstname'] . "\n" . "Last Name: " . $_POST['lastname'] . "\n" . "Barber: " . $_POST['barber'] . "\n" . "Date: " . $_POST['barber-appointment'] . "\r\n";
+  $fp = fopen('data.txt', 'a');
+  fwrite($fp, $data);
+  fclose($fp);
+  header("Location: appointments.html");
+?>
